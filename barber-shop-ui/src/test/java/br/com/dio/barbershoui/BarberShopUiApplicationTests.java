c59601ee326c5a71a1ee6f1341d1906e85d2a8b1
João Miccuci <joaomicucci3@gmail.com>
2025-04-04 15:26:12 -0300

    pasta back end

---
+++ barber-shop-ui/src/test/java/br/com/dio/barbershoui/BarberShopUiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dio.barbershoui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarberShopUiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
